40dc77f5c078cbdf00b614fd1687b6b64d60b0f0
LLDJ07 <lldj07@hotmail.com>
2025-03-19 14:27:42 +0100

    test

--- ZombieGameV4/Assets/turret.cs
+++ ZombieGameV4/Assets/turret.cs
@@ -1,16 +1,35 @@
 using UnityEngine;
 
-public class turret : MonoBehaviour
+public class Turret : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-       
-    }
-
-    // Update is called once per frame
     void Update()
     {
-        
+        GameObject closestEnemy = FindClosestEnemy();
+        if (closestEnemy != null)
+        {
+            Vector3 direction = (closestEnemy.transform.position - transform.position).normalized;
+            float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+            transform.rotation = Quaternion.Euler(0, 0, angle);
+        }
+    }
+
+    GameObject FindClosestEnemy()
+    {
+        GameObject[] enemies = GameObject.FindGameObjectsWithTag("Enemy");
+        GameObject closest = null;
+        float minDistance = Mathf.Infinity;
+        Vector3 position = transform.position;
+
+        foreach (GameObject enemy in enemies)
+        {
+            float distance = Vector3.Distance(position, enemy.transform.position);
+            if (distance < minDistance)
+            {
+                minDistance = distance;
+                closest = enemy;
+            }
+        }
+
+        return closest;
     }
 }
