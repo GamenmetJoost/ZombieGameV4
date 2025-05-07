5cce755266473d7a185a57b9aad527574e6b89f7
NeverEndingNiqht <fpjbarendsen@gmail.com>
2025-05-07 13:21:19 +0200

    Materials counter added

--- ZombieGameV4/Assets/Base.cs
+++ ZombieGameV4/Assets/Base.cs
@@ -1,3 +1,4 @@
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Base : MonoBehaviour
@@ -45,5 +46,12 @@ public class Base : MonoBehaviour
 
         // Display the HP in bright red letters
         GUI.Label(new Rect(10, 10, 200, 50), "HP: " + HP.ToString("F1"), style);
+        // Set the style for the text
+        style.fontSize = 24;
+        style.normal.textColor = Color.green;
+        // Get the material amount from the MaterialManager instance
+        int materialAmount = MaterialManager.Instance.materialAmount; // Assuming you have a MaterialManager script managing the materials
+        GUI.Label(new Rect(10, 30, 200, 50), "Materials: " + materialAmount.ToString("F1"), style);
+
     }
 }
