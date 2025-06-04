24c67b8b10f1a25da3f8ebbd0fe0d1a11c298f54
NeverEndingNiqht <fpjbarendsen@gmail.com>
2025-06-04 13:41:52 +0200

    StartScreen aangemaakt en ExitButton binnen GameOverScreen verwijderd

--- ZombieGameV4/Assets/GameOverScreen.cs
+++ ZombieGameV4/Assets/GameOverScreen.cs
@@ -11,13 +11,6 @@ public class GameOverScreen : MonoBehaviour
     public void RestartButton()
     {
         Time.timeScale = 1f; // Reset de tijd
-        SceneManager.LoadScene("SampleScene"); // Verwijder het sterretje!
-    }
-
-    public void ExitButton()
-    {
-        Time.timeScale = 1f;
-        Debug.Log("ExitButton pressed - Application.Quit() called");
-        Application.Quit();
+        SceneManager.LoadScene("SampleScene"); 
     }
 }
---
+++ ZombieGameV4/Assets/StartScreen.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartScreen : MonoBehaviour
+{
+    void Start()
+    {
+        Show_Screen();
+        Time.timeScale = 0f; // Pauzeer het spel bij start
+    }
+
+    public void Show_Screen()
+    {
+        gameObject.SetActive(true);
+    }
+
+    public void StartButton()
+    {
+        Time.timeScale = 1f; // Zet het spel weer aan
+        gameObject.SetActive(false); // Verberg het startscherm
+        // Als je NIET direct een nieuwe scene wilt laden, laat de volgende regel uitgeschakeld:
+        // SceneManager.LoadScene("SampleScene"); 
+    }
+
+    public void QuitButton()
+    {
+        Time.timeScale = 1f;
+        Debug.Log("ExitButton pressed - Application.Quit() called");
+        Application.Quit();
+    }
+}
